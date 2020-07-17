e876b5a294ffc052cc22e860f73c6a8df3411a52
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-17 16:44:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,11 +13,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {
-    :railroads => {
-      pieces: 4},
-      :names=>{ },
-  }
+monopoly = {:railroads => {pieces: 4, :names => {}, :rent_in_dollars => {}}}
 end
 
 #three_pieces_owned: 100, four-pieces_owned: 200}
